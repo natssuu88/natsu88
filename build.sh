5882468e9d933ab74cb95ac2b5f63b42789f330c
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-01-17 15:18:41 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/natssuu88/local_manifest --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/natssuu88/local_manifest --depth 1 -b nad-10-ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
-lunch banana_whyred-userdebug
+lunch nad_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka banana -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka nad -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
