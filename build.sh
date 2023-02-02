7028ba9b0ce88385e956cea7e0a970ab88decd98
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-02-02 16:52:11 +0700

    whyred: build: ArcaneOS v4.0 JINX

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/exthmui-legacy/android -b exthm-11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/natssuu88/local_manifest --depth 1 -b exthmui-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
+git clone https://github.com/natssuu88/local_manifest --depth 1 -b arcane-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
-source build/envsetup.sh
+. build/envsetup.sh
 export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
-lunch exthm_whyred-userdebug
+lunch aosp_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka bacon -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka bacon > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
