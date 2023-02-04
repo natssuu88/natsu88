77cda224fe979a19cee60b150a9a3576d64f1c25
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-02-04 15:44:37 +0700

    whyred: build: BananaDroid 11

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/natssuu88/local_manifest --depth 1 -b havoc-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/natssuu88/local_manifest --depth 1 -b banana-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,12 +11,12 @@ timeStart
 export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch havoc_whyred-userdebug
+lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-brunch whyred > reading & sleep 95m # Jangan di hapus text line (> reading)
+make banana > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
