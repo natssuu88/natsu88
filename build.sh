6276b12e175a6401ccee1b52b1b036dc5768a0b8
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-02-06 07:51:46 +0700

    whyred: build: ColtOS Enigma

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/natssuu88/local_manifest --depth 1 -b dot-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Colt-Enigma/platform_manifest -b c11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/natssuu88/local_manifest --depth 1 -b colt-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,12 +11,12 @@ timeStart
 export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch dot_whyred-userdebug
+lunch colt_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make bacon > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka colt > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
