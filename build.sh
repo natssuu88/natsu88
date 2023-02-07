a7f8a36bc2263c5c66070caee4a050ee21372f65
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-02-07 15:28:57 +0700

    whyred: build: BananaDroid v2.1 Susu

--- build.sh
+++ build.sh
@@ -1,22 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/natssuu88/local_manifest --depth 1 -b dot-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/namikazze/local_manifest --depth 1 -b banana-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
-. build/envsetup.sh
-export BUILD_USERNAME=$BUILD_USERNAME
-export BUILD_HOSTNAME=$BUILD_HOSTNAME
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch dot_whyred-userdebug
+source build/envsetup.sh
+export BUILD_USERNAME=namikazze
+export BUILD_HOSTNAME=android-build
+lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make bacon > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka banana -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
