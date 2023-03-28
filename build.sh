9de75fa82ac8bbb3a642ae8cce5e9314331f2f51
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-03-29 06:38:38 +0700

    build: whyred: HavocOS

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-brunch whyred > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka bacon -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
