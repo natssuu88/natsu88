15474c4fef47a19f6c9e20fdbbc975a02a6862e2
Natsu88 <122075751+natssuu88@users.noreply.github.com>
2023-03-30 20:19:31 +0700

    update [skip ci]

--- script/membangun.sh
+++ script/membangun.sh
@@ -5,16 +5,13 @@ name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
 command=$(tail $CIRRUS_WORKING_DIR/build.sh -n +$(expr $(grep '# build rom' $CIRRUS_WORKING_DIR/build.sh -n | cut -f1 -d:) - 1)| head -n -1 | grep -v '# end')
 cd $WORKDIR/rom/$name_rom
-#export ALLOW_MISSING_DEPENDENCIES=true
-#export PATH="/usr/lib/ccache:$PATH"
-#export CCACHE_DIR=$WORKDIR/ccache
-export CCACHE_DIR=~/ccache/$rom_name/$device
-export CCACHE_EXEC=$(which ccache)
+
+mkdir tempcc
 export USE_CCACHE=1
-#export CCACHE_COMPRESS=true
-#which ccache
-ccache -M 8G
-ccache -z
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_DIR=$PWD/tempcc
+ccache -M 100G -F 0
+
 bash $CIRRUS_WORKING_DIR/script/config
 bash -c "$command" || true
 bash $CIRRUS_WORKING_DIR/script/check_build.sh
